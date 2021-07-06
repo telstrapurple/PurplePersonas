769068e34a5c58ad5cdc535692bd0ed363e0f642
Trent Jones <trent.jones@purple.telstra.com>
2021-07-06 11:30:20 +0800

    Add logo and gorup of peeps

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,16 +18,21 @@
 </svelte:head>
 
 <div>
-	<h1>Purple Personas</h1>
-	<p>
-		Blurb about Purple Personas - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-		Pellentesque lobortis libero sollicitudin feugiat feugiat. Class aptent taciti sociosqu ad
-		litora torquent per conubia nostra, per inceptos himenaeos. In egestas erat nisl, at cursus
-		lacus luctus eu. Nam commodo metus sed est pulvinar euismod. Donec suscipit nec lacus vitae
-		imperdiet. Nam faucibus leo eget imperdiet posuere. Pellentesque gravida mauris vitae tortor
-		pulvinar efficitur. Phasellus sit amet bibendum sapien. Aliquam in placerat enim. Phasellus vel
-		accumsan velit. Maecenas placerat efficitur justo vel posuere.
-	</p>
+	<div class="flex flex-row">
+		<div class="flex flex-col">
+			<h1>Purple Personas</h1>
+			<p>
+				Blurb about Purple Personas - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Pellentesque lobortis libero sollicitudin feugiat feugiat. Class aptent taciti sociosqu ad
+				litora torquent per conubia nostra, per inceptos himenaeos. In egestas erat nisl, at cursus
+				lacus luctus eu. Nam commodo metus sed est pulvinar euismod. Donec suscipit nec lacus vitae
+				imperdiet. Nam faucibus leo eget imperdiet posuere. Pellentesque gravida mauris vitae tortor
+				pulvinar efficitur. Phasellus sit amet bibendum sapien. Aliquam in placerat enim. Phasellus
+				vel accumsan velit. Maecenas placerat efficitur justo vel posuere.
+			</p>
+		</div>
+		<img src={`${base}/people.svg`} alt="Group of diverse people" />
+	</div>
 	<h2>Our Roles</h2>
 	<div class="flex flex-row">
 		{#each roles as role}
