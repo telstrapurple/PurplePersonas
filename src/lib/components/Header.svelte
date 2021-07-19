e43f0cc0a1911c2e62bb16dc30cb9272b8547fd1
Trent Jones <trent.jones@purple.telstra.com>
2021-07-19 13:49:45 +0800

    Refine styles

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,27 +5,29 @@
 	export let portfolios: PortfolioDefinition[];
 </script>
 
-<div class="px-12 lg:px-48 py-6 lg:py-24">
+<div class="px-12 lg:px-32 py-6 lg:py-24">
 	<div class="max-w-screen-xl flex flex-col">
 		<div class="flex flex-col lg:flex-row">
 			<div class="flex flex-col">
 				<h1 class="pb-3 lg:pb-12">Purple Personas</h1>
-				<p>
+				<p class="max-w-prose">
 					This page describes each of the consulting roles at Telstra Purple in the Digital
 					portfolio (Development, Data and Design). It seeks to describe some defining
 					characteristics about each role to give our people and future Purplonians a clear
 					understanding of what we do.
 				</p>
 			</div>
-			<img src={`${base}/people.svg`} alt="Group of diverse people" />
+			<img src={`${base}/people.svg`} alt="Group of diverse people" class="max-h-300" />
 		</div>
-		<h2 class="py-3 lg:py-12">Our portfolios</h2>
-		<div class="flex flex-col lg:flex-row justify-between flex-wrap">
-			{#each portfolios as portfolio}
-				<a href={`${base}/${portfolio.slug}`} class="text-red">
-					<h3>{portfolio.metadata.title}</h3>
-				</a>
-			{/each}
-		</div>
+		{#if portfolios.length > 1}
+			<h2 class="py-3 lg:py-12">Our portfolios</h2>
+			<div class="flex flex-col lg:flex-row justify-between flex-wrap">
+				{#each portfolios as portfolio}
+					<a href={`${base}/${portfolio.slug}`} class="text-red">
+						<h3>{portfolio.metadata.title}</h3>
+					</a>
+				{/each}
+			</div>
+		{/if}
 	</div>
 </div>
--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -19,16 +19,16 @@
 	<title>{post.metadata.title}</title>
 </svelte:head>
 
-<div class="flex flex-col sm:flex-row px-12 lg:px-48 pt-4 lg:pt-14 bg-gray-lighter">
+<div class="flex flex-col sm:flex-row px-12 lg:px-32 pt-4 lg:pt-14 bg-gray-lighter">
 	<article class="portfolio">
-		<h2 class="pb-4 lg:pb-10">Our roles in {post.metadata.title}</h2>
+		<h2 class="pb-4 lg:pb-10 text-black text-xl">Our roles</h2>
 		<div class="flex flex-col lg:flex-row justify-around pb-4 lg:pb-14">
 			{#each post.metadata.roles.split(',') as role}
 				<a
 					href={`#${role.toLocaleLowerCase().replace(' ', '-')}-role-requirements`}
 					class="text-red"
 				>
-					<h3>{role}</h3>
+					<h3 class="text-lg">{role}</h3>
 				</a>
 			{/each}
 		</div>
