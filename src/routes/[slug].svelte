a2cb2a82afc25c6cef2d0105fb94d137c4a9f392
Trent Steenholdt <trent.steenholdt@readify.net>
2021-09-22 15:58:16 +0800

    Update css - max-w-full

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -36,7 +36,7 @@
 	<title>Purple Personas - {post.metadata.title}</title>
 </svelte:head>
 
-<div class="px-12 lg:px-32 max-w-screen bg-gray">
+<div class="px-12 lg:px-32 max-w-full bg-gray">
 	<h2 class="pb-4 lg:pb-4 text-black text-xl">Our roles</h2>
 	<div class="flex flex-col lg:flex-row justify-start pb-2 lg:pb-8">
 		{#each post.metadata.roles.split(',') as role}
